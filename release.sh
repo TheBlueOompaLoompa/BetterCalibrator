89f5cca51c518e7fa99b84277f6e1e99afac07f1
kuuuube <hexagonisalie@gmail.com>
2023-09-21 11:55:51 -0400

    Update release script

--- release.sh
+++ release.sh
@@ -8,8 +8,9 @@ cd tmp
 
 cp ../BetterCalibrator/bin/Release/net6.0/BetterCalibrator.dll .
 cp ../BetterCalibratorPopup/calibrator.x86_64 .
+cp ../BetterCalibratorPopup/calibrator.x86_64.exe .
 
-zip BetterCalibrator.zip BetterCalibrator.dll calibrator.x86_64
+zip BetterCalibrator.zip BetterCalibrator.dll calibrator.x86_64 calibrator.x86_64.exe
 
 mv BetterCalibrator.zip ../
 
